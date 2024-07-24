732487e57fe44106df894dedab7a00f8269d13cf
İlkay Şeki <sekiilkay@gmail.com>
2024-07-24 18:17:41 +0300

    [BE] AdvertList Completed

--- backend/ApsiyonKasif.Core/obj/Debug/net8.0/ApsiyonKasif.Core.AssemblyInfo.cs
+++ backend/ApsiyonKasif.Core/obj/Debug/net8.0/ApsiyonKasif.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApsiyonKasif.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99bcd1c9061f71e9c489f9e0ce56fc7163ffba79")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApsiyonKasif.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApsiyonKasif.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
